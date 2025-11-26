a372509ff1d3445ee144269938aa89d5b46849a1
plabondas001 <plabondas001@gmail.com>
2025-11-26 13:38:16 +0600

    lowest price

--- script.js
+++ script.js
@@ -19,3 +19,28 @@ function getMax(phones){
 
 let maxPrice = getMax(phones);
 console.log(maxPrice);
+
+
+// Lowest price
+
+
+let phones2 = [
+    {name: 'iphone', price: 100000, color: 'white',},
+    {name: 'realme', price: 20000, color: 'white',},
+    {name: 'oppo', price: 25000, color: 'white',},
+    {name: 'vivo', price: 27000, color: 'white',}
+
+]
+
+function getMin(phone){
+    let min = phone[0];
+    for(let mobile of phone){
+        if(mobile.price < min.price){
+            min = mobile
+        }
+    }
+    return min;
+}
+
+let minPrice = getMin(phones2);
+console.log('Lowest price: ', minPrice);
